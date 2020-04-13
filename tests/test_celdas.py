8cace55a1a572bf607e830dc85ab31496a39ff0c
Felipe <felipesagarraga@gmail.com>
2020-04-13 13:39:27 -0300

    Ejercicios tarea 1 hechos

--- tests/test_celdas.py
+++ tests/test_celdas.py
@@ -6,8 +6,32 @@ def test_contar_celdas_ocupadas():
     for fila in mi_carton:
         for celda in fila:
             contador = contador + celda
-    
+
     # Esperamos encontrar 15 celdas ocupadas.
     assert contador == 15
+def test_menos_de_15():
+    mi_carton = carton()
+    contador = 0
+    for fila in mi_carton:
+        for celda in fila:
+            contador = contador + celda
+    assert contador > 14
 
+def test_mas_de_15():
+    mi_carton = carton()
+    contador = 0
+    for fila in mi_carton:
+        for celda in fila:
+            contador = contador + celda
+    assert contador < 16
 
+columnas = [0,0,0,0,0,0,0,0,0]
+def test_columnas_ocupadas():
+    mi_carton = carton()
+    for fila in mi_carton:
+        contador = 0
+        for celda in fila:
+            if celda == 1:
+                columnas[contador] = 1
+            contador += 1
+    assert columnas == [1,1,1,1,1,1,1,1,1]
