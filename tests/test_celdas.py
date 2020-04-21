eb9793a45fefc03c98e8dda71e67391c984c65ac
Felipe <felipesagarraga@gmail.com>
2020-04-21 12:39:10 -0300

    Agrego comentario sobre funciones

--- tests/test_celdas.py
+++ tests/test_celdas.py
@@ -9,6 +9,8 @@ def test_contar_celdas_ocupadas():
 
     # Esperamos encontrar 15 celdas ocupadas.
     assert contador == 15
+
+#no tiene que haber menos de 15 celdas ocupadas
 def test_menos_de_15():
     mi_carton = carton()
     contador = 0
@@ -17,6 +19,7 @@ def test_menos_de_15():
             contador = contador + celda
     assert contador > 14
 
+#no tiene que haber mas de 15 celdas ocupadas
 def test_mas_de_15():
     mi_carton = carton()
     contador = 0
@@ -32,6 +35,6 @@ def test_columnas_ocupadas():
         contador = 0
         for celda in fila:
             if celda == 1:
-                columnas[contador] = 1
+                columnas[contador] = 1 #si encuentro un 1 se que la columna esta ocupada
             contador += 1
-    assert columnas == [1,1,1,1,1,1,1,1,1]
+    assert columnas == [1,1,1,1,1,1,1,1,1] #los 9 1 indican que todas las columnas tienen por lo menos una celda ocupada
